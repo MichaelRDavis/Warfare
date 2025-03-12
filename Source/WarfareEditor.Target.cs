8160d04ceddab4f946a338b27688e3eea6e869c5
Michael Davis <MichaelRDavis1991@protonmail.com>
2025-03-12 10:33:03 +0000

    Added C++ source code

---
+++ Source/WarfareEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class WarfareEditorTarget : TargetRules
+{
+	public WarfareEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+
+		ExtraModuleNames.AddRange( new string[] { "Warfare" } );
+	}
+}
